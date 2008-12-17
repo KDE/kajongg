6125cfd0dc2c624763d5240b55179c1051364996
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2008-12-17 03:17:17 +0000

    compile.sh adapted to KDE directory layout
    svn path=/trunk/playground/games/kmj/; revision=897892

--- compile.sh
+++ compile.sh
@@ -1,42 +1,26 @@
 #!/bin/sh
 
+# this file is only meant for the developer, please adapt
+# to your environment
+
 translate() {
-# http://www.gnu.org/software/autoconf/manual/gettext/xgettext-Invocation.html
-# http://api.kde.org/4.0-api/kdelibs-apidocs/kdecore/html/classKLocalizedString.html
-
-# extract messages from *.py
-	xgettext -ci18n --from-code=UTF-8 --language=Python -k \
-		-kki18n:1 -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 \
-		-ki18ncp:1c,2,3 -ktr2i18n:1 \
-		-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 \
-		-kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
-		--no-wrap --msgid-bugs-address=wolfgang@rohdewald.de -ol10n/kmj.pot *.py
-
-# merge new strings from kmj.pot into the language specific *.po:
-	cd l10n
-	catalogs=`find . -name '*.po'`
-	for cat in $catalogs; do
-		msgmerge -o $cat.new $cat kmj.pot
-		mv $cat.new $cat
-	done
-
-# translate
-	lokalize *.po
+	cd /home/wr/files/src/games-de
+	svn up
+	svn cat svn+ssh://wrohdewald@svn.kde.org/home/kde/trunk/l10n-kde4/templates/messages/playground-games/kmj.pot>kmj.pot
+	msgmerge -o kmj.new kmj.po kmj.pot
+	mv kmj.new kmj.po
+	lokalize kmj.po
 
 # compile and install the message files
-	msgfmt -o de.mo de.po
-	cp -a de.mo /home/wr/.kde/share/locale/de/LC_MESSAGES/kmj.mo
-	cd ..
+	msgfmt -o /home/wr/.kde/share/locale/de/LC_MESSAGES/kmj.mo kmj.po
 }
 
-
-#
-
+srcdir=`pwd -P`
 for i in *.ui
 do
 	pyuic4 $i > ${i%.ui}_ui.py
 done
-#pyrcc4 kmj.qrc >kmj_rc.py
 translate
+cd $srcdir
 #valgrind --trace-children=yes python kmj.py
 python kmj.py
