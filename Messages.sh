57d91547a28950be906b6195a00e5116a702b775
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2009-04-02 09:02:10 +0000

    Messages.sh can now run locally
    svn path=/trunk/playground/games/kmj/; revision=948101

--- Messages.sh
+++ Messages.sh
@@ -15,6 +15,6 @@ ${XGETTEXT:-xgettext} \
 		-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 \
 		-kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
 		-kRule:1 \
-		--no-wrap --msgid-bugs-address=wolfgang@rohdewald.de -o$podir/kmj.pot \
+		--no-wrap --msgid-bugs-address=wolfgang@rohdewald.de -o${podir:-.}/kmj.pot \
 		rc.cpp `find . -name \*.py`
 
