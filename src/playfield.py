f9b66f13e3e43ec272bdb00678819a2c4b2e605c
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2009-07-17 15:08:26 +0000

    version 0.3
    svn path=/trunk/playground/games/kmj/; revision=998394

--- src/playfield.py
+++ src/playfield.py
@@ -1697,7 +1697,7 @@ class About(object):
         self.catalog     = bytes('')
         self.homePage    = bytes('http://www.kde-apps.org/content/show.php/kmj?content=103206')
         self.bugEmail    = bytes('wolfgang@rohdewald.de')
-        self.version     = bytes('0.2')
+        self.version     = bytes('0.3')
         self.programName = ki18n ("kmj")
         self.description = ki18n ("kmj - computes payments among the 4 players")
         self.kmjlicense     = KAboutData.License_GPL
