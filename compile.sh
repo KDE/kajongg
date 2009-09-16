936c644dc009a47014d0e72cc62d772f70e39999
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2009-09-16 22:02:52 +0000

    add german user manual
    svn path=/trunk/playground/games/kmj/; revision=1024602

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@
 # this file is only meant for the developer, please adapt
 # to your environment
 
-translate() {
-	cd /home/wr/src/games-de
+translateMsg() {
+	cd /home/wr/src/games-msg
 	svn up
 	svn cat svn+ssh://wrohdewald@svn.kde.org/home/kde/trunk/l10n-kde4/templates/messages/playground-games/kmj.pot>kmj.pot
 	msgmerge -o kmj.new kmj.po kmj.pot
@@ -16,12 +16,26 @@ translate() {
 	sudo cp kmj.mo /usr/share/locale/de/LC_MESSAGES
 }
 
+translateDoc() {
+	cd /home/wr/src/games-doc
+	svn up
+	svn cat svn+ssh://wrohdewald@svn.kde.org/home/kde/trunk/l10n-kde4/templates/docmessages/playground-games/kmj.pot>kmj.pot
+	msgmerge -o kmj.new kmj.po kmj.pot
+	mv kmj.new kmj.po
+	lokalize kmj.po
+
+# compile and install the message files
+# po2xml
+	msgfmt -o kmj.mo kmj.po
+	sudo cp kmj.mo /usr/share/locale/de/LC_MESSAGES
+}
+
 cd $HOME/kmj/src
 for i in *.ui
 do
 	pyuic4 $i > ${i%.ui}_ui.py
 done
 cp kmjui.rc $HOME/.kde/share/apps/kmj/kmjui.rc
-translate
+translateMsg
 #valgrind --trace-children=yes python kmj.py
 #python kmj.py
