46013147eee581b95381ca0f7fef134b126ce7f4
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2009-09-17 21:33:43 +0000

    also prompts for doc translation into German
    svn path=/trunk/playground/games/kmj/; revision=1025044

--- compile.sh
+++ compile.sh
@@ -1,39 +1,49 @@
 #!/bin/sh
 
 # this file is only meant for the developer, please adapt
-# to your environment
+# to your environment. Helps in translation to german.
 
 translateMsg() {
-	cd /home/wr/src/games-msg
+	cd $HOME/src/kde/l10n-kde4/de/messages/playground-games
+	svn up  # we want the current kmj.po
 	svn cat svn://anonsvn.kde.org/home/kde/trunk/l10n-kde4/templates/messages/playground-games/kmj.pot>kmj.pot
-	msgmerge --previous -o kmj.new kmj.po kmj.pot
-	mv kmj.new kmj.po
+	msgmerge --update --previous kmj.po kmj.pot
 	lokalize kmj.po
+	rm kmj.pot
 
-# compile and install the message files
-	msgfmt -o kmj.mo kmj.po
-	sudo cp kmj.mo /usr/share/locale/de/LC_MESSAGES
 }
 
 translateDoc() {
-	cd /home/wr/src/games-doc
-	svn svn://anonsvn.kde.org/home/kde/trunk/l10n-kde4/templates/docmessages/playground-games/kmj.pot>kmj.pot
-	msgmerge --previous -o kmj.new kmj.po kmj.pot
-	mv kmj.new kmj.po
+	cd $HOME/src/kde/l10n-kde4/de/docmessages/playground-games
+	svn up  # we want the current kmj.po
+	svn cat svn://anonsvn.kde.org/home/kde/trunk/l10n-kde4/templates/docmessages/playground-games/kmj.pot>kmj.pot
+	msgmerge --update --previous kmj.po kmj.pot
 	lokalize kmj.po
+	rm kmj.pot
 
-# compile and install the message files
-# po2xml
-	msgfmt -o kmj.mo kmj.po
-	sudo cp kmj.mo /usr/share/locale/de/LC_MESSAGES
+# generate translated index.docbook
+	cd $HOME/src/kde/l10n-kde4
+	scripts/update_xml --nodelete de kmj
 }
 
-cd $HOME/kmj/src
+install() {
+	cd $HOME/src/kde/l10n-kde4/de/messages/playground-games
+	msgfmt -o kmj.mo kmj.po
+	sudo cp kmj.mo /usr/share/locale/de/LC_MESSAGES
+	rm kmj.mo
+	cd $HOME/src/kde/l10n-kde4/de/docs/playground-games/kmj/
+	sudo cp -a * /usr/share/doc/kde/HTML/de/kmj
+}
+
+cd $HOME/src/kde/playground/games/kmj/src
 for i in *.ui
 do
 	pyuic4 $i > ${i%.ui}_ui.py
 done
 cp kmjui.rc $HOME/.kde/share/apps/kmj/kmjui.rc
 translateMsg
+translateDoc
+install
+
 #valgrind --trace-children=yes python kmj.py
 #python kmj.py
