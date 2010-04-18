01b805383b5254985e62028e826c2e1c448de3ff
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2010-04-18 19:00:29 +0000

    sys.exit() hangs since updating from karmic 32 bit to lucid 64 bit. tried to update twisted 8.2 to 10.0 on karmic 32 but that still worked so the problem might not be twisted.
    this happens only when connecting via tcp to the server but not
    when connecting by socket.
    
    As a workaround use os._exit() which however does no cleanup like io flushing
    
    svn path=/trunk/KDE/kdegames/kajongg/; revision=1116157

--- src/playfield.py
+++ src/playfield.py
@@ -758,7 +758,9 @@ class PlayField(KXmlGuiWindow):
         """2nd stage: twisted reactor is already stopped"""
         StateSaver.saveAll()
         InternalParameters.app.quit()
-        sys.exit(0)
+ #       sys.exit(0)
+        os._exit(0) # TODO: should be sys.exit but that hangs since updating
+        # from karmic 32 bit to lucid 64 bit. os._exit does not clean up or flush buffers
 
     def closeEvent(self, event):
         if not self.quit():
