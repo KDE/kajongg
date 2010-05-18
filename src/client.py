KDE/kajongg

commit c81a5e7b3666188f9aa56aa38b4a1a47a6172e2d

If an invalid answer is entered, the timer no longer restarts if the active player selected an invalid answer, the next player continued without the wrongdoing player discarding a tile. It seems there are some points in Deferreds where calling QDialog or KMessageBox simply does nothing. This may have been different with older Qt / PyQt / twisted releases.
svn path=/trunk/KDE/kdegames/kajongg/; revision=1127609

src/humanclient.py
@@ -403,10 +403,11 @@ class ClientDialog(QDialog):
         self.connect(btn, SIGNAL('clicked(bool)'), self.selectedAnswer)
         self.buttons.append(btn)
 
-    def ask(self, answers, deferred):
+    def ask(self, move, answers, deferred):
         """make buttons specified by answers visible. The first answer is default.
         The default button only appears with blue border when this dialog has
         focus but we always want it to be recognizable. Hence setBackgroundRole."""
+        self.move = move
         self.deferred = deferred
         for answer in answers:
             self.__declareButton(answer)
@@ -481,10 +482,14 @@ class ClientDialog(QDialog):
     def selectButton(self, button=None):
         """select default answer"""
         if self.isVisible():
-            self.timer.stop()
             if button is None:
                 button = self.buttons[0]
             answer = Message.defined[str(button.objectName())]
+            if not self.client.maySay(self.move, answer):
+                message = m18n('You cannot say %1', answer.i18nName)
+                KMessageBox.sorry(None, message)
+                return
+            self.timer.stop()
             self.deferred.callback(answer)
         self.hide()
 
@@ -677,7 +682,8 @@ class HumanClient(Client):
             # reshowing it, sometimes the old buttons are still visible in which
             # case the next dialog will appear at a lower position than it should
             field.clientDialog = ClientDialog(self, field.centralWidget())
-        field.clientDialog.ask(answers, deferred)
+        assert field.clientDialog.client is self
+        field.clientDialog.ask(move, answers, deferred)
         self.answers.append(deferred)
 
     def selectChow(self, chows):
@@ -695,27 +701,16 @@ class HumanClient(Client):
         if InternalParameters.autoPlay:
             self.game.hidePopups()
             return Client.ask(self, move, answers)
-        print 'ich bin HumanClient.answered'
-        message = None
         myself = self.game.myself
-        try:
-            if answer == Message.Discard:
-                # do not remove tile from hand here, the server will tell all players
-                # including us that it has been discarded. Only then we will remove it.
-                myself.handBoard.setEnabled(False)
-                return answer.name, myself.handBoard.focusTile.element
-            args = self.maySay(move, answer)
-            if args:
-                return answer.name, args
-            else:
-                message = m18n('You cannot say %1', answer.i18nName)
-        finally:
-            if message:
-                KMessageBox.sorry(None, message)
-                InternalParameters.field.clientDialog.hide()
-                return self.ask(move, InternalParameters.field.clientDialog.answers)
-            else:
-                self.game.hidePopups()
+        if answer == Message.Discard:
+            # do not remove tile from hand here, the server will tell all players
+            # including us that it has been discarded. Only then we will remove it.
+            myself.handBoard.setEnabled(False)
+            return answer.name, myself.handBoard.focusTile.element
+        args = self.maySay(move, answer)
+        assert args
+        self.game.hidePopups()
+        return answer.name, args
 
     def remote_abort(self, tableid, message, *args):
         """the server aborted this game"""

commit 0f7d7740178ca6a0a0e8aadc9d2ee4fde69ffe51

Regression from r1127609: Do not ask twice for the wanted chow combination
svn path=/trunk/KDE/kdegames/kajongg/; revision=1128223

src/client.py
@@ -129,7 +129,7 @@ class Client(pb.Referenceable):
         answer = None
         for tryAnswer in [Message.MahJongg, Message.Kong, Message.Pung, Message.Chow]:
             if tryAnswer in answers:
-                sayable = self.maySay(move, tryAnswer)
+                sayable = self.maySay(move, tryAnswer, select=True)
                 if sayable:
                     answer = (tryAnswer, sayable)
                     break
@@ -225,14 +225,15 @@ class Client(pb.Referenceable):
                 if not belongsToPair:
                     return chow
 
-    def maySayChow(self):
+    def maySayChow(self, select=False):
         """returns answer arguments for the server if calling chow is possible.
         returns the meld to be completed"""
         game = self.game
         myself = game.myself
-        chows = myself.possibleChows(game.lastDiscard)
-        if chows:
-            return self.selectChow(chows)
+        result = myself.possibleChows(game.lastDiscard)
+        if result and select:
+            result = self.selectChow(result)
+        return result
 
     def maySayPung(self):
         """returns answer arguments for the server if calling pung is possible.
@@ -287,13 +288,13 @@ class Client(pb.Referenceable):
             lastMeld = list(hand.computeLastMeld(lastTile).pairs)
             return meldsContent(hand.hiddenMelds), withDiscard, lastMeld
 
-    def maySay(self, move, msg):
+    def maySay(self, move, msg, select=False):
         """returns answer arguments for the server if saying msg is possible"""
         # do not use a dict - most calls will be Pung
         if msg == Message.Pung:
             return self.maySayPung()
         if msg == Message.Chow:
-            return self.maySayChow()
+            return self.maySayChow(select)
         if msg == Message.Kong:
             return self.maySayKong()
         if msg == Message.MahJongg:

src/humanclient.py
@@ -707,7 +707,7 @@ class HumanClient(Client):
             # including us that it has been discarded. Only then we will remove it.
             myself.handBoard.setEnabled(False)
             return answer.name, myself.handBoard.focusTile.element
-        args = self.maySay(move, answer)
+        args = self.maySay(move, answer, select=True)
         assert args
         self.game.hidePopups()
         return answer.name, args

src/message.py
@@ -368,7 +368,6 @@ class MessageHasNoChoice(MessageFromServer):
         move.player.claimedNoChoice = True
         self.prevConc = move.player.concealedTiles
         move.player.setConcealedTiles(move.tile)
-        InternalParameters.autoPlay = False
         client.ask(move, [Message.OK], self.hideConcealedAgain)
 
     def hideConcealedAgain(self, dummy):

src/x.patch
@@ -1,0 +1,87 @@
+diff --git a/kajongg/src/sound.py b/kajongg/src/sound.py
+index ae96097..3d3518c 100644
+--- a/kajongg/src/sound.py
++++ b/kajongg/src/sound.py
+@@ -18,68 +18,37 @@ along with this program if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ """
+ 
+-import os, tarfile, time
++import os, tarfile
+ from hashlib import md5
+ 
+-from PyQt4.QtCore import SIGNAL, QProcess, QString, QStringList
++from PyKDE4.phonon import Phonon
+ 
+ import common
+-from util import which, logWarning, m18n, appdataDir, debugMessage
+-
+-
+-        # Phonon  does not work with short files - it plays them
+-        # simultaneously or only parts of them. Mar 2010, KDE 4.4. True for mp3
+-        # and for wav. Also, mpg123 often plays distorted sounds. Kubuntu 9.10.
+-        # So we use ogg123 and ogg sound files.
+-        # self.audio = Phonon.MediaObject(self)
+-        # self.audioOutput = Phonon.AudioOutput(Phonon.GameCategory, self)
+-        # Phonon.createPath(self.audio, self.audioOutput)
+-        # self.audio.enqueue(Phonon.MediaSource(wavName))
+-        # self.audio.play()
++from util import appdataDir
+ 
+ class Sound(object):
+     """the sound interface. Use class variables and class methods,
+     thusly ensuring no two instances try to speak"""
+     enabled = False
+-    __queue = []
+-    __process = None
+-    __hasogg123 = None
++    __cache = dict()
+ 
+     @staticmethod
+     def speak(what):
+         """this is what the user of this module will call."""
+         if not Sound.enabled:
+             return
+-        if Sound.__hasogg123 is None:
+-            if not which('ogg123'):
+-                Sound.enabled = False
+-                # checks again at next reenable
+-                logWarning(m18n('No voices will be heard because the program ogg123 is missing'))
+-                return
+-            Sound.__hasogg123 = True
+-        if Sound.__process:
+-            Sound.__queue.append(what)
++        if what in Sound.__cache:
++            entry = Sound.__cache[what]
+         else:
+-            Sound.__play(what)
++            if os.path.exists(what):
++                entry = Phonon.createPlayer(Phonon.GameCategory)
++                entry.setCurrentSource(Phonon.MediaSource(what))
++            else:
++                entry = None
++            Sound.__cache[what] = entry
++        if entry:
++            entry.play()
+ 
+-    @staticmethod
+-    def __play(what):
+-        """play what if it exists"""
+-        if os.path.exists(what):
+-            Sound.__process = QProcess()
+-            Sound.__process.connect(Sound.__process, SIGNAL('finished(int,QProcess::ExitStatus)'), Sound.__finished)
+-            args = QStringList('-q')
+-            args.append(what)
+-            Sound.__process.start(QString('ogg123'), args)
+-
+-    @staticmethod
+-    def __finished(code=None, status=None):
+-        """finished playing the sound"""
+-        Sound.__process = None
+-        if Sound.__queue:
+-            what = Sound.__queue[0]
+-            Sound.__queue = Sound.__queue[1:]
+-            Sound.__play(what)
+ 
+ class Voice(object):
+     """this administers voice sounds"""