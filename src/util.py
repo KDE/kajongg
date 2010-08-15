d8631ba3a387440d3492fb8bb855c16f76284dc4
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2010-08-15 10:02:17 +0000

    logWarning again...
    svn path=/trunk/KDE/kdegames/kajongg/; revision=1163890

--- src/util.py
+++ src/util.py
@@ -117,7 +117,7 @@ def debugMessage(msg):
 def logWarning(msg, prio=syslog.LOG_WARNING, isServer=False):
     """writes info message to syslog and to stdout"""
     if isinstance(msg, Exception):
-        msg = ' '.join(str(x) if isinstance(x, int, NoneType) else x for x in msg.args)
+        msg = ' '.join(str(x) for x in msg.args if x is not None)
     if isinstance(msg, str):
         msg = unicode(msg, 'utf-8')
     elif not isinstance(msg, unicode):
