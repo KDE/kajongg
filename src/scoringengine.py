00c2d48cb639e0aafffe28da75e76a2a1eed08c5
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2010-09-08 23:38:34 +0000

    SQL: writing an entire ruleset took up to 10 seconds. Speed up by putting all queries into one transaction
    svn path=/trunk/KDE/kdegames/kajongg/; revision=1173220

--- src/scoringengine.py
+++ src/scoringengine.py
@@ -27,7 +27,7 @@ from timeit import Timer
 
 from PyQt4.QtCore import QString
 
-from util import m18n, m18nc, english, logException, debugMessage
+from util import m18n, m18nc, english, logException, debugMessage, Duration
 from common import InternalParameters, elements
 from query import Query
 from tile import chiNext
@@ -362,16 +362,21 @@ class Ruleset(object):
         if not self.dirty and self.__used == self.orgUsed:
             # same content in same table
             return True
+        Query.dbhandle.transaction()
         self.remove()
         if not Query('INSERT INTO %s(id,name,hash,description) VALUES(?,?,?,?)' % self.__rulesetTable(),
             list([self.rulesetId, english(self.name), self.hash, self.description])).success:
+            Query.dbhandle.rollback()
             return False
         result = Query('INSERT INTO %s(ruleset, name, list, position, definition, '
                 'points, doubles, limits, parameter)'
                 ' VALUES(?,?,?,?,?,?,?,?,?)' % self.__ruleTable(),
                 self.ruleRecords()).success
         if result:
+            Query.dbhandle.commit()
             self.dirty = False
+        else:
+            Query.dbhandle.rollback()
         return result
 
     @staticmethod
