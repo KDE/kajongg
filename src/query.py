719e0e74e5d8516ec25feb7c738e6ff8caeea1bb
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2010-09-15 16:16:07 +0000

    Query: do not translate log message (Albert Astals Cid)
    svn path=/branches/KDE/4.5/kdegames/kajongg/; revision=1175703

--- src/query.py
+++ src/query.py
@@ -244,5 +244,5 @@ def initDb():
         Query("create index if not exists idxgame on score(game)")
 #        for table, field, what in [('server', 'lastruleset', 'text')]:
 #            if not Query.tableHasField(table, field):
-#                logMessage(m18n('adding missing field %s.%s' % (table,field)))
+#                logMessage('adding missing field %s.%s' % (table,field))
 #                Query(['alter table %s add column %s %s' % (table, field, what)])
