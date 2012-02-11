830cd28c1afce2bb1ede165478e523c2f514692c
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-02-11 11:33:21 +0000

    rename a variable
    AITile.preference -> AITile.keep  -  preference is too neutral
    
    SVN_SILENT
    
    svn path=/trunk/KDE/kdegames/kajongg/; revision=1279362

--- src/intelligence.py
+++ src/intelligence.py
@@ -55,26 +55,26 @@ class AIDefault:
                 # special case: do we have 4 consecutive singles?
                 values = list(set(int(x.name[1]) for x in colorCandidates))
                 if len(values) == 4 and values[0] + 3 == values[3]:
-                    colorCandidates[0].preference -= 5
+                    colorCandidates[0].keep -= 5
                     for candidate in colorCandidates[1:]:
-                        candidate.preference += 5
+                        candidate.keep += 5
                     break
             for prevCandidate, candidate, nextCandidate in self.runningWindow(colorCandidates, 3):
                 value = int(candidate.name[1])
                 prevValue = int(prevCandidate.name[1]) if prevCandidate else -99
                 nextValue = int(nextCandidate.name[1]) if nextCandidate else 99
                 if value == prevValue + 1:
-                    prevCandidate.preference += 1
-                    candidate.preference += 1
+                    prevCandidate.keep += 1
+                    candidate.keep += 1
                     if value == nextValue - 1:
-                        prevCandidate.preference += 2
-                        nextCandidate.preference += 2
+                        prevCandidate.keep += 2
+                        nextCandidate.keep += 2
                 if value == nextValue - 1:
-                    nextCandidate.preference += 1
-                    candidate.preference += 1
+                    nextCandidate.keep += 1
+                    candidate.keep += 1
                 if value == nextValue - 2:
-                    nextCandidate.preference += 0.5
-                    candidate.preference += 0.5
+                    nextCandidate.keep += 0.5
+                    candidate.keep += 0.5
 
     def selectDiscard(self):
         # pylint: disable=R0912, R0915
@@ -94,29 +94,29 @@ class AIDefault:
             groupCounts[tile[0]] += 1
             declaredGroupCounts[tile[0]] += 1
         for candidate in candidates:
-            preference = candidate.preference
+            keep = candidate.keep
             group, value = candidate.name
             candidate.occurrence = hiddenTiles.count(candidate.name)
             candidate.dangerous = bool(self.client.game.dangerousFor(self.client.game.myself, candidate.name))
             if candidate.dangerous:
-                preference += 1000
+                keep += 1000
             if candidate.occurrence >= 3:
-                preference += 10
+                keep += 10
             elif candidate.occurrence == 2:
-                preference += 5
-            preference += self.groupPrefs[group]
+                keep += 5
+            keep += self.groupPrefs[group]
             if group == 'w':
                 if value == hand.ownWind:
-                    preference += 1
+                    keep += 1
                 if value == hand.roundWind:
-                    preference += 1
+                    keep += 1
             if value in '19':
-                preference += 2
+                keep += 2
             if self.client.game.visibleTiles[candidate.name] == 3:
-                preference -= 10
+                keep -= 10
             elif self.client.game.visibleTiles[candidate.name] == 2:
-                preference -= 5
-            candidate.preference = preference
+                keep -= 5
+            candidate.keep = keep
         self._weighSameColors(candidates)
         for candidate in candidates:
             group = candidate.name[0]
@@ -124,23 +124,23 @@ class AIDefault:
             if group in 'sbc':
                 # count tiles with a different color:
                 if groupCount == 1:
-                    candidate.preference -= 2
+                    candidate.keep -= 2
                 else:
                     otherGC = sum(groupCounts[x] for x in 'sbc' if x != group)
                     if otherGC:
                         if groupCount > 8 or otherGC < 5:
                             # do not go for color game if we already declared something in another color:
                             if not any(declaredGroupCounts[x] for x in 'sbc' if x != group):
-                                candidate.preference += 20 // otherGC
+                                candidate.keep += 20 // otherGC
             elif group == 'w' and groupCount > 8:
-                candidate.preference += 10
+                candidate.keep += 10
             elif group == 'd' and groupCount > 7:
-                candidate.preference += 15
+                candidate.keep += 15
         self.weighCallingHand(hand, candidates)
-        candidates = sorted(candidates, key=lambda x: x.preference)
+        candidates = sorted(candidates, key=lambda x: x.keep)
         if Debug.robotAI:
             logDebug('%s: %s' % (self.client.game.myself, ' '.join(str(x) for x in candidates)))
-        # return tile with lowest preference:
+        # return tile with lowest keep:
         return candidates[0].name.capitalize()
 
     @staticmethod
@@ -151,7 +151,7 @@ class AIDefault:
             for winnerTile in newHand.isCalling(99):
                 string = newHand.string.replace(' m', ' M')
                 mjHand = HandContent.cached(newHand.ruleset, string, newHand.computedRules, plusTile=winnerTile)
-                candidate.preference -= mjHand.total() / 10
+                candidate.keep -= mjHand.total() / 10
 
     def selectAnswer(self, answers):
         """this is where the robot AI should go.
@@ -208,10 +208,10 @@ class TileAI(object):
         self.name = name
         self.occurrence = 0
         self.dangerous = False
-        self.preference = 0
+        self.keep = 0
 
     def __str__(self):
         dang = ' dang:%d' % self.dangerous if self.dangerous else ''
-        return '%s:=%d%s' % (self.name, self.preference, dang)
+        return '%s:=%d%s' % (self.name, self.keep, dang)
 
 INTELLIGENCES = {'Default': AIDefault}
