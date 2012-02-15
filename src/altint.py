0901309abda6c5bc7cbf5dcc8b8a37a8beba88d8
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-02-15 12:03:56 +0000

    altint.py: simplify skeleton
    SVN_SILENT
    
    svn path=/trunk/KDE/kdegames/kajongg/; revision=1280209

--- src/altint.py
+++ src/altint.py
@@ -18,7 +18,7 @@ along with this program if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """
 
-# from intelligence import AIDefault
-
-#class AIChow(AIDefault):
-#    """Chow alternative"""
+# pylint: disable=W0611
+# do not warn unused imports
+from intelligence import AIDefault
+from util import logDebug
