25cf00407e1848e5fdabb2369da305c024086798
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-04-04 14:48:08 +0000

    Rename Winding Snake to Squirming Snake
    svn path=/trunk/KDE/kdegames/kajongg/; revision=1288257

--- src/predefined.py
+++ src/predefined.py
@@ -179,7 +179,7 @@ class ClassicalChinese(PredefinedRuleset):
                 description=m18n('Only winds and dragons')))
         self.winnerRules.add(Rule('All Terminals', r'^((.[19]){1,4} )*[fy/]', limits=1,
                 description=m18n('Only 1 and 9')))
-        self.winnerRules.add(Rule('Winding Snake',
+        self.winnerRules.add(Rule('Squirming Snake',
                 r'I^(([sbc])1\2[1]\2[1] \2[2]\2[2] \2[3]\2[4]\2[5] \2[6]\2[7]\2[8] \2[9]\2[9]\2[9] [fy/])' \
                 r'|^(([sbc])1\4[1]\4[1] \4[2]\4[3]\4[4] \4[5]\4[5] \4[6]\4[7]\4[8] \4[9]\4[9]\4[9] [fy/])' \
                 r'|^(([sbc])1\6[1]\6[1] \6[2]\6[3]\6[4] \6[5]\6[6]\6[7] \6[8]\6[8] \6[9]\6[9]\6[9] [fy/])',
