KDE/kajongg

commit 506ac7d1319913845e50425feabb5eced6b7df9a

game with no winner should not be scored
svn path=/trunk/KDE/kdegames/kajongg/; revision=1254697

src/player.py
@@ -182,6 +182,8 @@ class Player(object):
             if self.hasManualScore():
                 spValue = InternalParameters.field.scoringDialog.spValues[self.idx]
                 return spValue.value()
+            if not self.game.winner:
+                return 0
             if self.handContent:
                 return self.handContent.total()
             return 0

commit cad053a04d84d89e98444a6b10e20f3c511930d5

fix a regression from r1254697
scoring manual game: update score again when changing hand tiles

svn path=/branches/KDE/4.8/kdegames/kajongg/; revision=1289377

src/player.py
@@ -182,7 +182,7 @@ class Player(object):
             if self.hasManualScore():
                 spValue = InternalParameters.field.scoringDialog.spValues[self.idx]
                 return spValue.value()
-            if not self.game.winner:
+            if not self.game.isScoringGame() and not self.game.winner:
                 return 0
             if self.handContent:
                 return self.handContent.total()