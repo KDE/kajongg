KDE/kajongg

commit 98d84e776432de451f24acb7241b69095400c69b

Chat: Fix problems if server goes away while chatting
Result was a nonresponsible client

svn path=/trunk/KDE/kdegames/kajongg/; revision=1300318

src/chat.py
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 from PyQt4.QtCore import Qt, QVariant, QAbstractTableModel, QModelIndex, QSize
 from PyQt4.QtGui import QWidget, QLineEdit, QVBoxLayout, QColor, QAbstractItemView
 
-from util import m18n, m18nE, logDebug
+from util import m18n, logDebug
 from guiutil import MJTableView
 from statesaver import StateSaver
 from message import ChatMessage
@@ -134,33 +134,44 @@ class ChatWindow(QWidget):
         self.show()
         StateSaver(self)
 
+    def show(self):
+        """not only show but also restore and raise"""
+        self.activateWindow()
+        self.setWindowState(self.windowState() & ~Qt.WindowMinimized)
+        self.raise_()
+        QWidget.show(self)
 
-    def sendLine(self):
+    def isVisible(self):
+        """not only visible but also not minimized"""
+        return QWidget.isVisible(self) and not self.windowState() & Qt.WindowMinimized
+
+    def kill(self):
+        """hide and null on table"""
+        print('chat.kill for %s on table %s' % (self, self.table))
+        self.hide()
+        self.table.chatWindow = None
+
+    def sendLine(self, line=None, isStatusMessage=False):
         """send line to others. Either the edited line or parameter line."""
-        line = unicode(self.edit.text())
+        if line is None:
+            line = unicode(self.edit.text())
+            self.edit.clear()
         if line:
             if Debug.chat:
                 logDebug('sending line %s to others' % line)
-            self.table.client.sendChat(ChatMessage(self.table.tableid, self.table.client.username, line))
-            self.edit.clear()
+            msg = ChatMessage(self.table.tableid, self.table.client.username, line, isStatusMessage)
+            self.table.client.sendChat(msg).addErrback(self.table.client.tableList.tableError)
+
+    def leave(self):
+        """leaving the chat"""
+        # TODO: send "left" message
+        self.hide()
 
     def receiveLine(self, chatLine):
         """show a new line in protocol"""
+        self.show()
         self.messageView.model().appendLines(chatLine)
         for row in range(self.messageView.model().rowCount()):
             self.messageView.setRowHeight(row, self.messageView.fontMetrics().height())
         self.messageView.resizeColumnsToContents()
         self.messageView.scrollToBottom()
-
-    @staticmethod
-    def createFor(table):
-        """create a chat window for table if it has none yet"""
-        if not table.chatWindow: # and isAlive(table.chatWindow):
-            table.chatWindow = ChatWindow(table)
-            table.chatWindow.activateWindow()
-            table.client.sendChat(ChatMessage(table.tableid, table.client.username,
-                m18nE('opens a chat window'), isStatusMessage=True))
-        table.chatWindow.setWindowState(table.chatWindow.windowState() & ~Qt.WindowMinimized)
-        table.chatWindow.show()
-        table.chatWindow.raise_()
-        return table.chatWindow

src/humanclient.py
@@ -817,8 +817,8 @@ class HumanClient(Client):
                 # TODO: chatting on a table with suspended game does
                 # not yet work because such a table has no tableid. Maybe it should.
                 return
-        if not chatLine.isStatusMessage:
-            ChatWindow.createFor(table)
+        if not chatLine.isStatusMessage and not table.chatWindow:
+            table.ChatWindow = ChatWindow(table)
         if table.chatWindow:
             table.chatWindow.receiveLine(chatLine)
 
@@ -1173,4 +1173,4 @@ class HumanClient(Client):
 
     def sendChat(self, chatLine):
         """send chat message to server"""
-        self.callServer('chat', chatLine.serialize())
+        return self.callServer('chat', chatLine.serialize())

src/tables.py
@@ -31,15 +31,15 @@ from kde import KApplication
 
 from genericdelegates import RichTextColumnDelegate
 
-from util import logWarning, m18n, m18nc, logDebug
+from util import logWarning, m18n, m18nc, m18nE, logDebug
 from statesaver import StateSaver
 from rule import Ruleset
 from guiutil import ListComboBox, MJTableView
 from differ import RulesetDiffer
 from common import InternalParameters, Debug
 from client import ClientTable
-from chat import ChatWindow
 from modeltest import ModelTest
+from chat import ChatMessage, ChatWindow
 
 class TablesModel(QAbstractTableModel):
     """a model for our tables"""
@@ -201,8 +201,23 @@ class TableList(QWidget):
         self.updateButtonsForTable(None)
 
     def chat(self):
-        """chat"""
-        ChatWindow.createFor(self.selectedTable())
+        """chat. Only generate ChatWindow after the
+        message has successfully been sent to the server.
+        Because the server might have gone away."""
+        def initChat(_):
+            """now that we were able to send the message to the server
+            instantiate the chat window"""
+            table.chatWindow = ChatWindow(table)
+            table.chatWindow.receiveLine(msg)
+        table = self.selectedTable()
+        if not table.chatWindow:
+            line = m18nE('opens a chat window')
+            msg = ChatMessage(table.tableid, table.client.username, line, isStatusMessage=True)
+            table.client.sendChat(msg).addCallback(initChat).addErrback(self.tableError)
+        elif table.chatWindow.isVisible():
+            table.chatWindow.hide()
+        else:
+            table.chatWindow.show()
 
     def show(self):
         """prepare the view and show it"""

commit a437e2765373c882f21a60352de7ac8a82b8ec94

Chat: fix a regression from r1300318
chatting from a running game opened a new chat window for every message

svn path=/trunk/KDE/kdegames/kajongg/; revision=1301435

src/chat.py
@@ -113,6 +113,7 @@ class ChatWindow(QWidget):
     def __init__(self, table=None, game=None):
         super(ChatWindow, self).__init__(None)
         self.table = table or game.client.table
+        self.table.chatWindow = self
         title = m18n('Chat on table %1 at %2', self.table.tableid, self.table.client.url)
         self.setObjectName('chatWindow')
         self.setWindowTitle(title + ' - kajongg')

src/humanclient.py
@@ -818,7 +818,7 @@ class HumanClient(Client):
                 # not yet work because such a table has no tableid. Maybe it should.
                 return
         if not chatLine.isStatusMessage and not table.chatWindow:
-            table.ChatWindow = ChatWindow(table)
+            ChatWindow(table)
         if table.chatWindow:
             table.chatWindow.receiveLine(chatLine)
 