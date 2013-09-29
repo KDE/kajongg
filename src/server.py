KDE/kajongg

commit ebc128248db0496ac35c69134fb82b0f3798ee78

Client starting a new table: Only send ruleset hash to server
but not the full ruleset definition. If the server does not yet know this ruleset, ask the client for
the full definition

src/server.py
@@ -57,6 +57,7 @@ from message import Message, ChatMessage
 from common import elements, Debug
 from sound import Voice
 from deferredutil import DeferredBlock
+from rule import Ruleset
 
 def srvMessage(*args):
     """concatenate all args needed for m18n encoded in one string.
@@ -870,13 +871,25 @@ class MJServer(object):
 
     def newTable(self, user, ruleset, playOpen, autoPlay, wantedGame, tableId=None):
         """user creates new table and joins it"""
-        def sent(dummy):
-            """new table sent to user who created it"""
-            return table.tableid
+        def gotRuleset(ruleset):
+            """now we have the full ruleset definition from the client"""
+            Ruleset.cached(ruleset).save(copy=True) # make it known to the cache and save in db
         if tableId in self.tables:
             return fail(srvError(pb.Error,
                 'You want a new table with id=%d but that id is already used for table %s' % (
                     tableId, self.tables[tableId])))
+        if Ruleset.hashIsKnown(ruleset):
+            self.__newTable(None, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
+        else:
+            self.callRemote(user, 'needRuleset', ruleset).addCallback(
+                gotRuleset).addCallback(
+                self.__newTable, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
+
+    def __newTable(self, dummy, user, ruleset, playOpen, autoPlay, wantedGame, tableId=None):
+        """now we know the ruleset"""
+        def sent(dummy):
+            """new table sent to user who created it"""
+            return table.tableid
         table = ServerTable(self, user, ruleset, None, playOpen, autoPlay, wantedGame, tableId)
         result = None
         for srvUser in self.srvUsers:

src/humanclient.py
@@ -515,6 +515,13 @@ class HumanClient(Client):
         else:
             self.__receiveTables(tables)
 
+    @staticmethod
+    def remote_needRuleset(ruleset):
+        """server only knows hash, needs full definition"""
+        result = Ruleset.cached(ruleset)
+        assert result and result.hash == ruleset
+        return result.toList()
+
     def tableChanged(self, table):
         """update table list"""
         oldTable, newTable = Client.tableChanged(self, table)
@@ -779,7 +786,7 @@ class HumanClient(Client):
         """as the name says"""
         if ruleset is None:
             ruleset = self.ruleset
-        return self.callServer('newTable', ruleset.toList(), Options.playOpen,
+        return self.callServer('newTable', ruleset.hash, Options.playOpen,
             Internal.autoPlay, self.__wantedGame(), tableid).addErrback(self.tableError)
 
     def newTable(self):

commit 853539d1962f2fc2ce633af0bbe6ffcdb3b8e4e5

fix recent regression from ebc1282

src/server.py
@@ -879,9 +879,9 @@ class MJServer(object):
                 'You want a new table with id=%d but that id is already used for table %s' % (
                     tableId, self.tables[tableId])))
         if Ruleset.hashIsKnown(ruleset):
-            self.__newTable(None, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
+            return self.__newTable(None, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
         else:
-            self.callRemote(user, 'needRuleset', ruleset).addCallback(
+            return self.callRemote(user, 'needRuleset', ruleset).addCallback(
                 gotRuleset).addCallback(
                 self.__newTable, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
 