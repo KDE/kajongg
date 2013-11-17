KDE/kajongg

commit db66b78ca747d71a642546d821889c05b4e9a96b

better text for explaining scores. New unique key for Rule

src/predefined.py
@@ -173,35 +173,35 @@ class ClassicalChinese(PredefinedRuleset):
             'with one of them')))
 
         # doubling melds:
-        self.meldRules.add(Rule('Pung/Kong of Dragons', 'FDragonPungKong', doubles=1))
-        self.meldRules.add(Rule('Pung/Kong of Own Wind', 'FOwnWindPungKong', doubles=1))
-        self.meldRules.add(Rule('Pung/Kong of Round Wind', 'FRoundWindPungKong', doubles=1))
+        self.meldRules.add(Rule('{meldName}', 'FDragonPungKong', doubles=1))
+        self.meldRules.add(Rule('{meldType} of Own Wind ({value})', 'FOwnWindPungKong', doubles=1))
+        self.meldRules.add(Rule('{meldType} of Round Wind ({value})', 'FRoundWindPungKong', doubles=1))
 
         # exposed melds:
-        self.meldRules.add(Rule('Exposed Kong', 'FExposedMinorKong', points=8))
-        self.meldRules.add(Rule('Exposed Kong of Terminals', 'FExposedTerminalsKong', points=16))
-        self.meldRules.add(Rule('Exposed Kong of Honors', 'FExposedHonorsKong', points=16))
+        self.meldRules.add(Rule('{meldName}', 'FExposedMinorKong', points=8))
+        self.meldRules.add(Rule('{meldName}', 'FExposedTerminalsKong', points=16))
+        self.meldRules.add(Rule('{meldName}', 'FExposedHonorsKong', points=16))
 
-        self.meldRules.add(Rule('Exposed Pung', 'FExposedMinorPung', points=2))
-        self.meldRules.add(Rule('Exposed Pung of Terminals', 'FExposedTerminalsPung', points=4))
-        self.meldRules.add(Rule('Exposed Pung of Honors', 'FExposedHonorsPung', points=4))
+        self.meldRules.add(Rule('{meldName}', 'FExposedMinorPung', points=2))
+        self.meldRules.add(Rule('{meldName}', 'FExposedTerminalsPung', points=4))
+        self.meldRules.add(Rule('{meldName}', 'FExposedHonorsPung', points=4))
 
         # concealed melds:
-        self.meldRules.add(Rule('Concealed Kong', 'FConcealedMinorKong', points=16))
-        self.meldRules.add(Rule('Concealed Kong of Terminals', 'FConcealedTerminalsKong', points=32))
-        self.meldRules.add(Rule('Concealed Kong of Honors', 'FConcealedHonorsKong', points=32))
+        self.meldRules.add(Rule('{meldName}', 'FConcealedMinorKong', points=16))
+        self.meldRules.add(Rule('{meldName}', 'FConcealedTerminalsKong', points=32))
+        self.meldRules.add(Rule('{meldName}', 'FConcealedHonorsKong', points=32))
 
-        self.meldRules.add(Rule('Concealed Pung', 'FConcealedMinorPung', points=4))
-        self.meldRules.add(Rule('Concealed Pung of Terminals', 'FConcealedTerminalsPung', points=8))
-        self.meldRules.add(Rule('Concealed Pung of Honors', 'FConcealedHonorsPung', points=8))
+        self.meldRules.add(Rule('{meldName}', 'FConcealedMinorPung', points=4))
+        self.meldRules.add(Rule('{meldName}', 'FConcealedTerminalsPung', points=8))
+        self.meldRules.add(Rule('{meldName}', 'FConcealedHonorsPung', points=8))
 
-        self.meldRules.add(Rule('Pair of Own Wind', 'FOwnWindPair', points=2))
-        self.meldRules.add(Rule('Pair of Round Wind', 'FRoundWindPair', points=2))
-        self.meldRules.add(Rule('Pair of Dragons', 'FDragonPair', points=2))
+        self.meldRules.add(Rule('Pair of Own Wind ({value})', 'FOwnWindPair', points=2))
+        self.meldRules.add(Rule('Pair of Round Wind ({value})', 'FRoundWindPair', points=2))
+        self.meldRules.add(Rule('{meldName}', 'FDragonPair', points=2))
 
         # bonus tiles:
-        self.meldRules.add(Rule('Flower', 'FFlower', points=4))
-        self.meldRules.add(Rule('Season', 'FSeason', points=4))
+        self.meldRules.add(Rule('{meldName}', 'FFlower', points=4))
+        self.meldRules.add(Rule('{meldName}', 'FSeason', points=4))
 
 class ClassicalChineseDMJL(ClassicalChinese):
     """classical chinese rules, German rules"""
@@ -229,7 +229,7 @@ class ClassicalChineseDMJL(ClassicalChinese):
         self.handRules.add(Rule('Big Four Joys', 'FBigFourJoys', doubles=2,
                 description=m18n('4 Pungs or Kongs of winds')))
 
-        self.winnerRules['Only Honors'].doubles = 2
+        self.winnerRules['OnlyHonors'].doubles = 2
 
         self.penaltyRules.add(Rule('False Naming of Discard, Claimed for Chow', points = -50))
         self.penaltyRules.add(Rule('False Naming of Discard, Claimed for Pung/Kong', points = -100))
@@ -256,16 +256,16 @@ class ClassicalChineseBMJA(ClassicalChinese):
     def addParameterRules(self):
         """those differ for BMJA from standard"""
         ClassicalChinese.addParameterRules(self)
-        self.parameterRules['Size of Kong Box'].parameter = 14
-        self.parameterRules['number of allowed chows'].parameter = 1
-        self.parameterRules['Points for a Limit Hand'].parameter = 1000
-        self.parameterRules['must declare calling hand'].parameter = True
+        self.parameterRules['kongBoxSize'].parameter = 14
+        self.parameterRules['maxChows'].parameter = 1
+        self.parameterRules['limit'].parameter = 1000
+        self.parameterRules['mustDeclareCallingHand'].parameter = True
 
     def loadRules(self):
 # TODO: we need a separate part for any number of announcements. Both r for robbing kong and a for
 # Original Call can be possible together.
         ClassicalChinese.loadRules(self)
-        del self.winnerRules['Zero Point Hand']
+        del self.winnerRules['ZeroPointHand']
         originalCall = self.winnerRules.pop('Mah Jongg with Original Call')
         originalCall.name = m18nE('Original Call')
         self.handRules.add(originalCall)
@@ -281,30 +281,30 @@ class ClassicalChineseBMJA(ClassicalChinese):
                 description=m18n('7 pairs of tiles in any 2 out of 3 suits; no Winds or Dragons')))
         self.mjRules.add(Rule('All pair honors', 'FAllPairHonors', limits=0.5,
                 description=m18n('7 pairs of 1s/9s/Winds/Dragons')))
-        del self.handRules['Own Flower and Own Season']
-        del self.handRules['Three Concealed Pongs']
+        del self.handRules['OwnFlowerOwnSeason']
+        del self.handRules['ThreeConcealedPongs']
         self.handRules.add(Rule('Own Flower', 'FOwnFlower', doubles=1))
         self.handRules.add(Rule('Own Season', 'FOwnSeason', doubles=1))
         del self.winnerRules['Last Tile Taken from Dead Wall']
-        del self.winnerRules['Hidden Treasure']
-        del self.winnerRules['False Color Game']
-        del self.winnerRules['Concealed True Color Game']
+        del self.winnerRules['HiddenTreasure']
+        del self.winnerRules['FalseColorGame']
+        del self.winnerRules['ConcealedTrueColorGame']
         del self.winnerRules['East won nine times in a row']
-        del self.winnerRules['Last Tile Completes Pair of 2..8']
-        del self.winnerRules['Last Tile Completes Pair of Terminals or Honors']
-        del self.winnerRules['Last Tile is Only Possible Tile']
+        del self.winnerRules['LastTileCompletesPairMinor']
+        del self.winnerRules['LastTileCompletesPairMajor']
+        del self.winnerRules['LastOnlyPossible']
         self.winnerRules.add(Rule('Buried Treasure', 'FBuriedTreasure', limits=1,
                 description=m18n('Concealed pungs of one suit with winds/dragons and a pair')))
-        del self.winnerRules['True Color Game']
+        del self.winnerRules['TrueColorGame']
         self.winnerRules.add(Rule('Purity', 'FPurity', doubles=3,
                 description=m18n('Only same-colored tiles (no chows, dragons or winds)')))
-        self.winnerRules['All Greens'].name = m18nE('Imperial Jade')
+        self.winnerRules['AllGreen'].name = m18nE('Imperial Jade')
         self.mjRules['Thirteen Orphans'].name = m18nE('The 13 Unique Wonders')
-        del self.winnerRules['Three Great Scholars']
-        self.winnerRules.add(Rule('Three Great Scholars', 'FThreeGreatScholars||Onochow', limits=1,
-                description=m18n('3 Pungs or Kongs of dragons plus any pung/kong and a pair')))
-        self.handRules['All Flowers'].score.doubles = 2
-        self.handRules['All Seasons'].score.doubles = 2
+        self.winnerRules['ThreeGreatScholars'] = Rule('Three Great Scholars',
+                'FThreeGreatScholars||Onochow', limits=1,
+                description=m18n('3 Pungs or Kongs of dragons plus any pung/kong and a pair'))
+        self.handRules['AllFlowers'].score.doubles = 2
+        self.handRules['AllSeasons'].score.doubles = 2
         self.penaltyRules.add(Rule('False Naming of Discard, Claimed for Chow/Pung/Kong', points = -50))
         self.penaltyRules.add(Rule('False Declaration of Mah Jongg by One Player',
                 'Oabsolute payees=3', limits = -0.5))

src/rule.py
@@ -161,40 +161,46 @@ class RuleList(list):
         self.name = name
         self.description = description
 
-    def pop(self, name):
+    def pop(self, key):
         """find rule, return it, delete it from this list"""
-        result = self.__getitem__(name)
-        self.__delitem__(name)
+        result = self.__getitem__(key)
+        self.__delitem__(key)
         return result
 
-    def __getitem__(self, name):
-        """find rule by name"""
-        if isinstance(name, int):
-            return list.__getitem__(self, name)
+    def __contains__(self, key):
+        """do we know this rule?"""
+        if isinstance(key, Rule):
+            key = key.key()
+        return any(x.key() == key for x in self)
+
+    def __getitem__(self, key):
+        """find rule by key"""
+        if isinstance(key, int):
+            return list.__getitem__(self, key)
         for rule in self:
-            if rule.name == name:
+            if rule.key() == key:
                 return rule
         raise KeyError
 
-    def __setitem__(self, name, rule):
-        """set rule by name"""
+    def __setitem__(self, key, rule):
+        """set rule by key"""
         assert isinstance(rule, Rule)
-        if isinstance(name, int):
-            list.__setitem__(self, name, rule)
+        if isinstance(key, int):
+            list.__setitem__(self, key, rule)
             return
         for idx, oldRule in enumerate(self):
-            if oldRule.name == name:
+            if oldRule.key() == key:
                 list.__setitem__(self, idx, rule)
                 return
         list.append(self, rule)
 
-    def __delitem__(self, name):
+    def __delitem__(self, key):
         """delete this rule"""
-        if isinstance(name, int):
-            list.__delitem__(self, name)
+        if isinstance(key, int):
+            list.__delitem__(self, key)
             return
         for idx, rule in enumerate(self):
-            if rule.name == name:
+            if rule.key() == key:
                 list.__delitem__(self, idx)
                 return
         raise KeyError
@@ -205,7 +211,10 @@ class RuleList(list):
 
     def add(self, rule):
         """use add instead of append"""
-        self[rule.name] = rule
+        if rule.key() in self:
+            logException('%s is already defined as %s, not accepting new rule %s/%s'% (
+                rule.key(), self[rule.key()].definition, rule.name, rule.definition))
+        self[rule.key()] = rule
 
 class Ruleset(object):
     """holds a full set of rules: splitRules,meldRules,handRules,winnerRules.
@@ -379,6 +388,7 @@ into a situation where you have to pay a penalty"""))
         for par in self.parameterRules:
             self.__dict__[par.parName] = par.parameter
         for ruleList in self.ruleLists:
+            assert len(ruleList) == len(set(x.key() for x in ruleList)), '%s has non-unique key' % ruleList.name
             for rule in ruleList:
                 rule.score.ruleset = self
                 self.allRules.append(rule)
@@ -674,6 +684,23 @@ class Rule(object):
                 break
         self.definition = definition
 
+    def key(self):
+        """the key is used for finding a rule in a RuleList. Since we do not
+        want to manually define a unique key for every rule, this is a bit
+        complicated:
+        - if the definition starts with F and contains no ||, key is rule.definition
+          without the leading F
+        - if the definition defines a parameter, the parType at the beginning is stripped
+          and the first part before || is used
+        - otherwise key is rule.name"""
+        if not self._definition:
+            return self.name
+        if self._definition[0] == 'F' and '||' not in self._definition:
+            return self._definition[1:]
+        elif self.parType:
+            return self._definition.split('||')[0]
+        return self.name
+
     @property
     def definition(self):
         """the rule definition. See user manual about ruleset."""
@@ -758,9 +785,15 @@ class Rule(object):
         """those might be candidates for a calling hand"""
         return set()
 
-    def explain(self):
+    def explain(self, meld):
         """use this rule for scoring"""
-        return m18n(self.name) + ': ' + self.score.contentStr()
+        return '%s: %s' % ( m18n(self.name).format(
+            group=meld[0].groupName() if meld else '',
+            value=meld[0].valueName() if meld else '',
+            meldType=meld.typeName() if meld else '',
+            meldName=meld.name() if meld else '',
+            tileName=meld[0].name() if meld else ''
+            ).replace('&', '').replace('  ', ' ').strip(), self.score.contentStr())
 
     def hashStr(self):
         """all that is needed to hash this rule. Try not to change this to keep

src/hand.py
@@ -715,11 +715,11 @@ class Hand(object):
 
     def explain(self):
         """explain what rules were used for this hand"""
-        result = [x.rule.explain() for x in self.usedRules
+        result = [x.rule.explain(x.meld) for x in self.usedRules
             if x.rule.score.points]
-        result.extend([x.rule.explain() for x in self.usedRules
+        result.extend([x.rule.explain(x.meld) for x in self.usedRules
             if x.rule.score.doubles])
-        result.extend([x.rule.explain() for x in self.usedRules
+        result.extend([x.rule.explain(x.meld) for x in self.usedRules
             if not x.rule.score.points and not x.rule.score.doubles])
         if any(x.rule.debug for x in self.usedRules):
             result.append(str(self))

src/query.py
@@ -80,8 +80,7 @@ class DBHandle(QSqlDatabase):
             doubles text,
             limits text,
             parameter text,
-            primary key(ruleset,list,position),
-            unique (ruleset,name)"""
+            primary key(ruleset,list,position)"""
     schema['server'] = """
                 url text,
                 lastname text,
@@ -212,6 +211,17 @@ class DBHandle(QSqlDatabase):
                     'drop table ruleset',
                     self.sqlForCreateTable('ruleset'),
                     'insert into ruleset select * from temp',
+                    'drop table temp'])
+        query = Query("select count(1) from sqlite_master "
+            "where type='table' and tbl_name='rule' and sql like '%unique (ruleset,name)%'", silent=True)
+        if int(query.records[0][0]):
+            # make ruleset,name non-unique
+            Query([
+                    'create table temp(%s)' % self.schema['rule'],
+                    'insert into temp select * from rule',
+                    'drop table rule',
+                    self.sqlForCreateTable('rule'),
+                    'insert into rule select * from temp',
                     'drop table temp'])
 
     def upgradeDb(self):

src/rulecode.py
@@ -26,6 +26,7 @@ from meld import Meld, CONCEALED, EXPOSED, CLAIMEDKONG, REST, elementKey
 from common import IntDict, WINDS
 from message import Message
 from query import Query
+from util import logDebug
 
 class Function(object):
     """Parent for all Function classes. We need to implement

src/scoring.py
@@ -537,14 +537,14 @@ class ExplainView(QListView):
             lines.append(m18n('There is no active game'))
         else:
             i18nName = m18n(self.game.ruleset.name)
-            lines.append(m18n('Ruleset: %1', i18nName))
+            lines.append(m18n('%1', i18nName))
             lines.append('')
             for player in self.game.players:
                 pLines = []
                 if player.hand and player.hand.hasTiles():
                     total = player.hand.total()
                     if total:
-                        pLines = ['%s: %s' % (player.localName, total)]
+                        pLines = ['%s - %s' % (player.localName, total)]
                         for line in player.hand.explain():
                             pLines.append('- ' + line)
                 elif player.handTotal:

commit 2ec41529c15acd47176928e457b377fbdcd9922a

show correct rule names in ruleset editor again
fixing a regression from db66b78ca7, Oct 25 2013

src/predefined.py
@@ -173,35 +173,35 @@ class ClassicalChinese(PredefinedRuleset):
             'with one of them')))
 
         # doubling melds:
-        self.meldRules.add(Rule('{meldName}', 'FDragonPungKong', doubles=1))
-        self.meldRules.add(Rule('{meldType} of Own Wind ({value})', 'FOwnWindPungKong', doubles=1))
-        self.meldRules.add(Rule('{meldType} of Round Wind ({value})', 'FRoundWindPungKong', doubles=1))
+        self.meldRules.add(Rule('Pung/Kong of Dragons', 'FDragonPungKong', explainTemplate='{meldName}', doubles=1))
+        self.meldRules.add(Rule('Pung/Kong of Own Wind', 'FOwnWindPungKong', explainTemplate='{meldType} of Own Wind ({value})', doubles=1))
+        self.meldRules.add(Rule('Pung/Kong of Round Wind', 'FRoundWindPungKong', explainTemplate='{meldType} of Round Wind ({value})', doubles=1))
 
         # exposed melds:
-        self.meldRules.add(Rule('{meldName}', 'FExposedMinorKong', points=8))
-        self.meldRules.add(Rule('{meldName}', 'FExposedTerminalsKong', points=16))
-        self.meldRules.add(Rule('{meldName}', 'FExposedHonorsKong', points=16))
+        self.meldRules.add(Rule('Exposed Kong', 'FExposedMinorKong', explainTemplate='{meldName}', points=8))
+        self.meldRules.add(Rule('Exposed Kong of Terminals', 'FExposedTerminalsKong', explainTemplate='{meldName}', points=16))
+        self.meldRules.add(Rule('Exposed Kong of Honors', 'FExposedHonorsKong', explainTemplate='{meldName}', points=16))
 
-        self.meldRules.add(Rule('{meldName}', 'FExposedMinorPung', points=2))
-        self.meldRules.add(Rule('{meldName}', 'FExposedTerminalsPung', points=4))
-        self.meldRules.add(Rule('{meldName}', 'FExposedHonorsPung', points=4))
+        self.meldRules.add(Rule('Exposed Pung', 'FExposedMinorPung', explainTemplate='{meldName}', points=2))
+        self.meldRules.add(Rule('Exposed Pung of Terminals', 'FExposedTerminalsPung', explainTemplate='{meldName}', points=4))
+        self.meldRules.add(Rule('Exposed Pung of Honors', 'FExposedHonorsPung', explainTemplate='{meldName}', points=4))
 
         # concealed melds:
-        self.meldRules.add(Rule('{meldName}', 'FConcealedMinorKong', points=16))
-        self.meldRules.add(Rule('{meldName}', 'FConcealedTerminalsKong', points=32))
-        self.meldRules.add(Rule('{meldName}', 'FConcealedHonorsKong', points=32))
+        self.meldRules.add(Rule('Concealed Kong', 'FConcealedMinorKong', explainTemplate='{meldName}', points=16))
+        self.meldRules.add(Rule('Concealed Kong of Terminals', 'FConcealedTerminalsKong', explainTemplate='{meldName}', points=32))
+        self.meldRules.add(Rule('Concealed Kong of Honors', 'FConcealedHonorsKong', explainTemplate='{meldName}', points=32))
 
-        self.meldRules.add(Rule('{meldName}', 'FConcealedMinorPung', points=4))
-        self.meldRules.add(Rule('{meldName}', 'FConcealedTerminalsPung', points=8))
-        self.meldRules.add(Rule('{meldName}', 'FConcealedHonorsPung', points=8))
+        self.meldRules.add(Rule('Concealed Pung', 'FConcealedMinorPung', explainTemplate='{meldName}', points=4))
+        self.meldRules.add(Rule('Concealed Pung of Terminals', 'FConcealedTerminalsPung', explainTemplate='{meldName}', points=8))
+        self.meldRules.add(Rule('Concealed Pung of Honors', 'FConcealedHonorsPung', explainTemplate='{meldName}', points=8))
 
-        self.meldRules.add(Rule('Pair of Own Wind ({value})', 'FOwnWindPair', points=2))
-        self.meldRules.add(Rule('Pair of Round Wind ({value})', 'FRoundWindPair', points=2))
-        self.meldRules.add(Rule('{meldName}', 'FDragonPair', points=2))
+        self.meldRules.add(Rule('Pair of Own Wind', 'FOwnWindPair', explainTemplate='Pair of Own Wind ({value})', points=2))
+        self.meldRules.add(Rule('Pair of Round Wind', 'FRoundWindPair', explainTemplate='Pair of Round Wind ({value})', points=2))
+        self.meldRules.add(Rule('Pair of Dragons', 'FDragonPair', explainTemplate='{meldName}', points=2))
 
         # bonus tiles:
-        self.meldRules.add(Rule('{meldName}', 'FFlower', points=4))
-        self.meldRules.add(Rule('{meldName}', 'FSeason', points=4))
+        self.meldRules.add(Rule('Flower', 'FFlower', explainTemplate='{meldName}', points=4))
+        self.meldRules.add(Rule('Season', 'FSeason', explainTemplate='{meldName}', points=4))
 
 class ClassicalChineseDMJL(ClassicalChinese):
     """classical chinese rules, German rules"""

src/rule.py
@@ -659,11 +659,12 @@ class Rule(object):
     # pylint: disable=too-many-arguments,too-many-instance-attributes
 
     def __init__(self, name, definition='', points = 0, doubles = 0, limits = 0, parameter = None,
-            description=None, debug=False):
+            description=None, explainTemplate=None, debug=False):
         self.options = {}
         self.function = None
         self.hasSelectable = False
         self.name = name
+        self.explainTemplate = explainTemplate
         self.description = description
         self.score = Score(points, doubles, limits)
         self._definition = None
@@ -787,12 +788,13 @@ class Rule(object):
 
     def explain(self, meld):
         """use this rule for scoring"""
-        return '%s: %s' % ( m18n(self.name).format(
-            group=meld[0].groupName() if meld else '',
-            value=meld[0].valueName() if meld else '',
-            meldType=meld.typeName() if meld else '',
-            meldName=meld.name() if meld else '',
-            tileName=meld[0].name() if meld else ''
+        return '%s: %s' % ( m18n(
+            self.explainTemplate if self.explainTemplate else self.name).format(
+                group=meld[0].groupName() if meld else '',
+                value=meld[0].valueName() if meld else '',
+                meldType=meld.typeName() if meld else '',
+                meldName=meld.name() if meld else '',
+                tileName=meld[0].name() if meld else ''
             ).replace('&', '').replace('  ', ' ').strip(), self.score.contentStr())
 
     def hashStr(self):