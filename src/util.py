KDE/kajongg

commit 2dca8e384ee3dfff1a5d922c51dcb3b1674620ec

xToUtf8 did not convert QString(msg) to str

src/util.py
@@ -199,7 +199,9 @@ def xToUtf8(msg, args=None):
     """makes sure msg and all args are utf-8"""
     if isinstance(msg, unicode):
         msg = msg.encode('utf-8')
-    if args:
+    elif not isinstance(msg, str):
+        msg = str(msg)
+    if args is not None:
         args = list(args[:])
         for idx, arg in enumerate(args):
             if isinstance(arg, unicode):

commit 83dcf98d7f0f0b899c672a88e50097a1374494bb

regression from 2dca8e384ee3: logging of QString strings

src/util.py
@@ -215,14 +215,14 @@ def xToUtf8(msg, args=None):
     if isinstance(msg, unicode):
         msg = msg.encode('utf-8')
     elif not isinstance(msg, str):
-        msg = str(msg)
+        msg = unicode(msg).encode('utf-8')
     if args is not None:
         args = list(args[:])
         for idx, arg in enumerate(args):
             if isinstance(arg, unicode):
                 args[idx] = arg.encode('utf-8')
             elif not isinstance(arg, str):
-                args[idx] = str(arg)
+                args[idx] = unicode(arg).encode('utf-8')
         return msg, args
     else:
         return msg