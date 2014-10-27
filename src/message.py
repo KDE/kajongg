dcb4e3870d5177ed655b772dd2c429baa89ff07b
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2014-10-27 15:22:36 +0100

    Message.jelly: send tile strings as bytes

--- src/message.py
+++ src/message.py
@@ -67,7 +67,7 @@ class Message(object):
         # pylint: disable=too-many-return-statements
         cls = value.__class__
         if cls in (Tile, TileList, Meld, MeldList):
-            return str(value)
+            return str(value).encode()
         elif isinstance(value, Message):
             return value.name
         elif isinstance(value, (list, tuple)):
