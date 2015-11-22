dc266864f2263ee96ef11b2e963334d8b0683636
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2015-11-22 16:00:15 +0100

    GatesOfHeaven/NineGates: ComputeLastMelds may only return existing melds

--- src/rulecode.py
+++ src/rulecode.py
@@ -1002,10 +1002,9 @@ class EastWonNineTimesInARow(RuleCode):
 class GatesOfHeaven(StandardMahJongg):
     cache = ()
 
-# FIXME: computeLastMelds must return a real meld
 # FIXME: in BMJA, 111 and 999 must be concealed, we do not check this
     def computeLastMelds(hand):
-        return [hand.lastTile.single]
+        return [x for x in hand.melds if hand.lastTile in x]
 
     def shouldTry(hand, maxMissing=3):
         if hand.declaredMelds:
