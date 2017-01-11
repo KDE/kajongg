ac615e8dba8dbf90df5664413d8e2924034145ea
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2017-01-11 23:26:46 +0100

    remove a wrong assertion
    triggered by a user of kajongg.org, found in the server log

--- src/kdestub.py
+++ src/kdestub.py
@@ -108,7 +108,6 @@ def i18n(englishIn, *args):
     @return: The translated text, args included.
     @rtype: C{unicode}
     """
-    assert englishIn
     if not Debug.neutral and KGlobal.translation and englishIn:
         _ = KGlobal.translation.gettext(englishIn)
     else:
