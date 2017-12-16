216f265292cae796959ae42c6db99b9ecfd74380
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2017-12-16 10:17:12 +0100

    SPDX-License-Identifier

--- src/genericdelegates.py
+++ src/genericdelegates.py
@@ -2,14 +2,8 @@
 Copyright (c) 2007-2008 Qtrac Ltd <mark@qtrac.eu>
 Copyright (C) 2008-2016 Wolfgang Rohdewald <wolfgang@rohdewald.de>
 
- This program or module is free software: you can redistribute it and/or
- modify it under the terms of the GNU General Public License as published
- by the Free Software Foundation, either version 2 of the License, or
- version 3 of the License, or (at your option) any later version. It is
- provided for educational purposes and is distributed in the hope that
- it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- the GNU General Public License for more details.
+SPDX-License-Identifier: GPL-2.0
+
 """
 
 from qt import Qt, QSize, QRect, QEvent
