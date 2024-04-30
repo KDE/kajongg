KDE/kajongg

commit b1d3de8df066e2acd8b9e28e9b5e8982f2ccb631

use logException for all missing addErrback

src/games.py
@@ -234,4 +234,4 @@ class Games(QDialog):
                 "Do you really want to delete <numid>%1</numid> games?<br>"
                 "This will be final, you cannot cancel it with "
                 "the cancel button",
-                len(deleteGames))).addCallback(answered, deleteGames)
+                len(deleteGames))).addCallback(answered, deleteGames).addErrback(logException)

src/client.py
@@ -379,7 +379,7 @@ class Client(pb.Referenceable):
                         'wrong token: %s, we have %s' %
                         (move.token, self.game.handId.token()))
         with Duration('Move {!r}:'.format(move)):
-            return self.exec_move(move).addCallback(self.__jellyMessage)
+            return self.exec_move(move).addCallback(self.__jellyMessage).addErrback(logException)
 
     def exec_move(self, move):
         """mirror the move of a player as told by the game server"""
@@ -432,7 +432,7 @@ class Client(pb.Referenceable):
             return answer
         # return answer only after animation ends. Put answer into
         # the Deferred returned by animate().
-        return animate().addCallback(lambda x: answer)
+        return animate().addCallback(lambda x: answer).addErrback(logException)
 
     def claimed(self, move):
         """somebody claimed a discarded tile"""

src/deferredutil.py
@@ -404,7 +404,7 @@ class DeferredBlock(ReprMixin):
             isClient = rec.__class__.__name__.endswith('Client')
             if isClient:
                 defer = Deferred()
-                defer.addCallback(rec.remote_move, command, **kwargs)
+                defer.addCallback(rec.remote_move, command, **kwargs).addErrback(logException)
                 defer.command = command.name
                 defer.notifying = 'notifying' in kwargs
                 self.__addRequest(defer, rec, about)

src/game.py
@@ -508,7 +508,7 @@ class Game:
         self.clearHand()
         if self.finished():
             if Options.rounds:
-                self.close().addCallback(Internal.mainWindow.close)
+                self.close().addCallback(Internal.mainWindow.close).addErrback(logException)
             else:
                 self.close()
 

src/humanclient.py
@@ -419,7 +419,7 @@ class HumanClient(Client):
                     client.logout(
                     ).addCallback(
                         disconnectedClient,
-                        client))
+                        client).addErrback(logException))
         return DeferredList(deferreds)
 
     def __loggedIn(self, connection):
@@ -548,8 +548,8 @@ class HumanClient(Client):
             self.callServer(
                 'needRulesets',
                 needRulesets).addCallback(
-                    gotRulesets).addCallback(
-                        self.__receiveTables)
+                    gotRulesets).addErrback(logException).addCallback(
+                        self.__receiveTables).addErrback(logException)
         else:
             self.__receiveTables(tables)
 
@@ -583,7 +583,7 @@ class HumanClient(Client):
                         if self.beginQuestion:
                             self.beginQuestion.cancel()
                         Sorry(i18n('Player %1 has left the table', name)).addCallback(
-                            sorried).addCallback(self.showTableList)
+                            sorried).addErrback(logException).addCallback(self.showTableList).addErrback(logException)
                         break
         self.__updateTableList()
         return oldTable, newTable
@@ -655,7 +655,7 @@ class HumanClient(Client):
                 Internal.mainWindow.updateGUI()
         assert self.game
         assert not self.game.isFirstHand()
-        return Information(i18n("Ready for next hand?"), modal=False).addCallback(answered)
+        return Information(i18n("Ready for next hand?"), modal=False).addCallback(answered).addErrback(logException)
 
     def ask(self, move, answers):
         """server sends move. We ask the user. answers is a list with possible answers,
@@ -780,7 +780,7 @@ class HumanClient(Client):
         if Internal.scene:
             # update the balances in the status bar:
             Internal.scene.mainWindow.updateGUI()
-        Information(i18n(message, *args)).addCallback(yes)
+        Information(i18n(message, *args)).addCallback(yes).addErrback(logException)
 
     def remote_serverDisconnects(self, result=None):
         """we logged out or lost connection to the server.
@@ -894,7 +894,7 @@ class HumanClient(Client):
             self.connection = None
             if Debug.connections:
                 logDebug('sending logout to server for {}'.format(self))
-            return self.callServer('logout').addCallback(loggedout, conn)
+            return self.callServer('logout').addCallback(loggedout, conn).addErrback(logException)
         return succeed(None)
 
     def __logCallServer(self, *args):

src/message.py
@@ -492,7 +492,8 @@ class MessageReadyForGameStart(ServerMessage):
         assert move.gameid
         return client.readyForGameStart(  # type:ignore[attr-defined]
             move.tableid, move.gameid,
-            move.wantedGame, move.playerNames, shouldSave=move.shouldSave).addCallback(hideTableList)
+            move.wantedGame, move.playerNames, shouldSave=move.shouldSave).addCallback(
+                hideTableList).addErrback(logException)
 
 
 class MessageNoGameStart(NotifyAtOnceMessage):
@@ -801,7 +802,7 @@ class MessageNoChoice(ServerMessage):
         # otherwise we have a visible artifact of the discarded tile.
         # Only when animations are disabled. Why?
 #        Internal.mainWindow.centralView.resizeEvent(None)
-        return client.ask(move, [Message.OK]).addCallback(self.hideConcealedAgain)
+        return client.ask(move, [Message.OK]).addCallback(self.hideConcealedAgain).addErrback(logException)
 
     def hideConcealedAgain(self, result):
         """only show them for explaining the 'no choice'"""

src/scene.py
@@ -9,7 +9,7 @@ SPDX-License-Identifier: GPL-2.0
 
 from twisted.internet.defer import succeed, Deferred
 
-from log import logDebug
+from log import logDebug, logException
 from mi18n import i18n, i18nc
 from common import LIGHTSOURCES, Internal, isAlive, ZValues, Debug
 from common import ReprMixin, Speeds, id4
@@ -515,7 +515,8 @@ class ScoringScene(GameScene):
         if self.game.finished():
             self.game = None
             return succeed(True)
-        return QuestionYesNo(i18n("Do you really want to abort this game?"), always=True).addCallback(answered)
+        return QuestionYesNo(i18n("Do you really want to abort this game?"), always=True).addCallback(
+            answered).addErrback(logException)
 
     def __moveTile(self, uiTile, wind, toConcealed):
         """the user pressed a wind letter or X for center, wanting to move a uiTile there"""

src/server.py
@@ -63,7 +63,7 @@ Internal.reactor = reactor
 
 from player import Players
 from query import Query, initDb
-from log import logDebug, logWarning, logError, logInfo, SERVERMARK
+from log import logDebug, logWarning, logError, logInfo, logException, SERVERMARK
 from mi18n import i18n, i18nE
 from util import elapsedSince
 from message import Message, ChatMessage
@@ -104,7 +104,7 @@ class DBPasswordChecker:
             return fail(credError.UnauthorizedLogin(srvMessage(template, cred.username)))
         userid, password = query.records[0]
         defer1 = maybeDeferred(cred.checkPassword, password.encode('utf-8'))
-        defer1.addCallback(DBPasswordChecker._checkedPassword, userid)
+        defer1.addCallback(DBPasswordChecker._checkedPassword, userid).addErrback(logException)
         return defer1
 
     @staticmethod
@@ -234,8 +234,8 @@ class MJServer:
         if Ruleset.hashIsKnown(ruleset):
             return self.__newTable(None, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
         return self.callRemote(user, 'needRuleset', ruleset).addCallback(
-            gotRuleset).addCallback(
-                self.__newTable, user, ruleset, playOpen, autoPlay, wantedGame, tableId)
+            gotRuleset).addErrback(logException).addCallback(
+                self.__newTable, user, ruleset, playOpen, autoPlay, wantedGame, tableId).addErrback(logException)
 
     def __newTable(self, unused, user, ruleset,
                    playOpen, autoPlay, wantedGame, tableId=None):
@@ -257,7 +257,7 @@ class MJServer:
             deferred = self.sendTables(srvUser, [table])
             if user == srvUser:
                 result = deferred
-                deferred.addCallback(sent)
+                deferred.addCallback(sent).addErrback(logException)
         assert result
         return result
 

commit 93ce4013a3e3e3f6ed31d5e97f4fe1c131b380a9

Regression from b1d3de8, 2023-10-05: Cancel deleting game must not throw exception

src/games.py
@@ -19,7 +19,6 @@ from qt import QItemSelectionModel, QAbstractItemView
 from dialogs import WarningYesNo
 from kde import KIcon
 from mi18n import i18n, i18nc
-from log import logException
 from query import Query
 from guiutil import MJTableView, decorateWindow
 from statesaver import StateSaver
@@ -219,9 +218,9 @@ class Games(QDialog):
 
     def delete(self) ->None:
         """delete a game"""
-        def answered(result:bool, games:List[int]) ->None:
+        def answered(result:Any, games:List[int]) ->None:
             """question answered, result is True or False"""
-            if result:
+            if result is True:
                 for game in games:
                     Query("DELETE FROM score WHERE game = ?", (game, ))
                     Query("DELETE FROM game WHERE id = ?", (game, ))
@@ -235,4 +234,4 @@ class Games(QDialog):
                 "Do you really want to delete <numid>%1</numid> games?<br>"
                 "This will be final, you cannot cancel it with "
                 "the cancel button",
-                len(deleteGames))).addCallback(answered, deleteGames).addErrback(logException)
+                len(deleteGames))).addBoth(answered, deleteGames)