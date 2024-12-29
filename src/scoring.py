e675d08b4acb07aae4afc3c0c91abb429e9a3230
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2024-12-29 11:27:01 +0100

    Revert "close/open db connection per game"
    This reverts commit 2c2810117455883d04eab5088f3651b2c8c8df8c.
    
    The correct solution would be separate data bases for every client and server

--- src/scoring.py
+++ src/scoring.py
@@ -22,7 +22,7 @@ from wind import Wind
 from tilesource import TileSource
 from animation import animate
 from log import logError, logDebug, logWarning, i18n
-from query import Query, openDb
+from query import Query
 from uitile import UITile, UIMeld
 from board import WindLabel, Board
 from game import Game
@@ -652,7 +652,6 @@ class ScoringGame(Game):
 
 def scoreGame() ->Optional[ScoringGame]:
     """show all games, select an existing game or create a new game"""
-    openDb()
     Players.load()
     if len(Players.humanNames) < 4:
         logWarning(
