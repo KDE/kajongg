8dc1f79780a68ce19ae3fa1d27d81303cc9b400a
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2024-12-30 23:00:11 +0100

    yellow indicator for prevailing wind did not work for a very long time

--- src/visible.py
+++ src/visible.py
@@ -88,7 +88,7 @@ class VisiblePlayer(Player):
             assert _
             side.disc = _
         assert self.game
-        side.disc.is_prevailing = self.game.roundsFinished > 0
+        side.disc.is_prevailing = self.wind == self.game.roundWind
         side.disc.board = self.front
 
 
