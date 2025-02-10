KDE/kajongg

commit 1e15493604637fe1d98f82477af4ec4910df2aeb

mypy: also silence warnings from Pyside6 bindings

src/scoringdialog.py
@@ -36,7 +36,7 @@ from wind import Wind
 from tile import Tile, MeldList
 
 if TYPE_CHECKING:
-    from qt import QObject, QRect, QStyleOptionViewItem
+    from qt import QObject, QRect, QStyleOptionViewItem, QPersistentModelIndex
     from tile import Meld
     from rule import Rule, Score
     from scene import ScoringScene
@@ -148,15 +148,19 @@ class ScoreItemDelegate(QStyledItemDelegate):
     def __init__(self, parent:Optional['QObject']=None) ->None:
         QStyledItemDelegate.__init__(self, parent)
 
-    def paint(self, painter:QPainter, option:'QStyleOptionViewItem', index:QModelIndex) ->None:
+    def paint(self, painter:QPainter, option:'QStyleOptionViewItem',
+        index:Union[QModelIndex,'QPersistentModelIndex']) ->None:
         """where the real work is done..."""
+        assert isinstance(index, ScorePlayerItem)
         item = index.internalPointer()
         if isinstance(item, ScorePlayerItem) and item.parent and item.parent.row() == 3 and index.column() != 0:
-            for idx, playerItem in enumerate(index.parent().internalPointer().children):
-                chart = cast('ScoreModel', index.model()).chart(option.rect, index, playerItem)
+            parent_item = cast(TreeItem, index.parent().internalPointer())
+            for idx, playerItem in enumerate(parent_item.children):
+                rect = option.rect  # type:ignore[attr-defined]
+                chart = cast('ScoreModel', index.model()).chart(rect, index, playerItem)
                 if chart:
                     with Painter(painter):
-                        painter.translate(option.rect.topLeft())
+                        painter.translate(rect.topLeft())
                         painter.setPen(self.colors[idx])
                         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
                         # if we want to use a pen width > 1, we can no longer directly drawPolyline
@@ -192,13 +196,14 @@ class ScoreModel(TreeModel):
         xValues = [x * stepX for x in range(self.steps + 1)]
         return [QPointF(x, y) for x, y in zip(xValues, yValues)]
 
-    def data(self, index:QModelIndex, role:int=Qt.ItemDataRole.DisplayRole) ->Any:  # pylint: disable=too-many-branches
+    def data(self, index:Union[QModelIndex,'QPersistentModelIndex'], role:int=Qt.ItemDataRole.DisplayRole) ->Any:  # pylint: disable=too-many-branches
         """score table"""
         # pylint: disable=too-many-return-statements
+        assert isinstance(index, QModelIndex)
         if not index.isValid():
             return None
         column = index.column()
-        item = index.internalPointer()
+        item = cast(TreeItem, index.internalPointer())
         assert item.parent
         if role == Qt.ItemDataRole.DisplayRole:
             if isinstance(item, ScorePlayerItem):
@@ -628,7 +633,10 @@ class PenaltyBox(QSpinBox):
 
     def validate(self, inputData:str, pos:int) -> Tuple[QValidator.State, str, int]:
         """check if value is a multiple of parties"""
-        result, inputData, newPos = QSpinBox.validate(self, inputData, pos)
+        _ = QSpinBox.validate(self, inputData, pos)
+        result = _[0]
+        inputData = _[1]
+        newPos = _[2]
         if result == QValidator.State.Acceptable:
             try:
                 int_data = int(inputData)

src/animation.py
@@ -213,7 +213,6 @@ class ParallelAnimationGroup(QParallelAnimationGroup, ReprMixin):
         assert self.state() != QAbstractAnimation.State.Running
         for animation in self.animations:
             graphicsObject = animation.targetObject()
- #            graphicsObject = cast(AnimatedMixin, animation.targetObject())
             if not isAlive(animation) or not isAlive(graphicsObject):
                 return fail()
             animatedObject = cast(AnimatedMixin, graphicsObject)
@@ -224,7 +223,7 @@ class ParallelAnimationGroup(QParallelAnimationGroup, ReprMixin):
             if propName == 'rotation':
                 # change direction if that makes the difference smaller
                 endValue = animation.endValue()
-                currValue = animatedObject.rotation
+                currValue = cast(float, animatedObject.rotation)
                 if endValue - currValue > 180:
                     animation.setStartValue(currValue + 360)
                 if currValue - endValue > 180:
@@ -351,10 +350,15 @@ class AnimatedMixin:
         self.queuedAnimations = []
         self.setDrawingOrder()  # type:ignore[attr-defined] # TODO: mypy protocol?
 
-    def getValue(self, pName:str) ->PropertyType:
+    def getValue(self, pName:str) ->Union[QPointF,int,float]:
         """get a current property value"""
-        return {'pos': self.pos, 'rotation': self.rotation,
-                'scale': self.scale}[pName]
+        if pName == 'pos':
+            return cast(QPointF, self.pos)
+        if pName == 'rotation':
+            return cast(int, self.rotation)
+        if pName == 'scale':
+            return cast(float, self.scale)
+        assert False
 
     def setActiveAnimation(self, animation:'Animation') ->None:
         """the graphics object knows which of its properties are currently animated"""

src/background.py
@@ -63,7 +63,7 @@ class Background(Resource):
             width = self.imageWidth
             height = self.imageHeight
         cachekey = f'{self.name}W{width}H{height}'
-        result = QPixmapCache.find(cachekey)
+        result = QPixmapCache.find(cachekey)  # type:ignore[call-overload]
         if not result:
             renderer = QSvgRenderer(self.graphicsPath)
             if not renderer.isValid():

src/board.py
@@ -39,7 +39,7 @@ if TYPE_CHECKING:
     from scene import SceneWithFocusRect
 
 
-class WindDisc(DrawOnTopMixin, AnimatedMixin, QGraphicsObject, ReprMixin):
+class WindDisc(DrawOnTopMixin, AnimatedMixin, QGraphicsObject, ReprMixin):  # type:ignore[misc]
 
     """a round wind tile"""
 
@@ -374,7 +374,7 @@ class Board(QGraphicsRectItem, ReprMixin):
                 _ = uiTile.board.tileFaceRect().bottomRight()
                 view = Internal.scene.mainWindow.centralView
                 menuPoint = view.mapToGlobal(
-                    view.mapFromScene(uiTile.mapToScene(_)))
+                    view.mapFromScene(cast(QPointF, uiTile.mapToScene(_))))
             action = menu.exec(menuPoint)
             if not action:
                 return None
@@ -891,8 +891,8 @@ class FittingView(QGraphicsView):
         tRect = uiTile.boundingRect()
         tRect = self.viewportTransform().mapRect(tRect)
         pmapSize = QSize(
-            int(tRect.width() * uiTile.scale),
-            int(tRect.height() * uiTile.scale))
+            int(tRect.width() * cast(float, uiTile.scale)),
+            int(tRect.height() * cast(float, uiTile.scale)))
         pMap = uiTile.pixmapFromSvg(pmapSize)
         drag.setPixmap(pMap)
         drag.setHotSpot(QPoint(pMap.width() // 2, pMap.height() // 2))

src/chat.py
@@ -9,7 +9,7 @@ SPDX-License-Identifier: GPL-2.0
 
 from typing import List, Optional, Union, TYPE_CHECKING, cast
 
-from qt import Qt, QAbstractTableModel, QModelIndex, QSize
+from qt import Qt, QAbstractTableModel, QModelIndex, QPersistentModelIndex, QSize
 from qt import QWidget, QLineEdit, QVBoxLayout, QColor, QAbstractItemView
 
 from log import i18n, logDebug
@@ -51,7 +51,7 @@ class ChatModel(QAbstractTableModel):
             result = [i18n('Time'), i18n('Player'), i18n('Message')][section]
         return result
 
-    def rowCount(self, parent:QModelIndex=QModelIndex()) ->int:
+    def rowCount(self, parent:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->int:
         """how many lines are in the model?"""
         if parent.isValid():
             # similar in tables.py
@@ -59,11 +59,12 @@ class ChatModel(QAbstractTableModel):
             return 0
         return len(self.chatLines)
 
-    def columnCount(self, unusedParent:QModelIndex=QModelIndex()) ->int:
+    def columnCount(self, unusedParent:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->int:
         """for now we only have time, who, message"""
         return 3
 
-    def data(self, index:QModelIndex, role:int=Qt.ItemDataRole.DisplayRole) ->Union[int, str, QColor, None]:
+    def data(self, index:Union[QModelIndex,QPersistentModelIndex],
+        role:int=Qt.ItemDataRole.DisplayRole) ->Union[int, str, QColor, None]:
         """score table"""
         result:Union[int, str, QColor, None] = None
         if role == Qt.ItemDataRole.TextAlignmentRole:

src/differ.py
@@ -9,7 +9,7 @@ SPDX-License-Identifier: GPL-2.0
 
 from typing import List, Tuple, TYPE_CHECKING, Optional, Union
 
-from qt import Qt, QAbstractTableModel, QModelIndex
+from qt import Qt, QAbstractTableModel, QModelIndex, QPersistentModelIndex
 from qt import QLabel, QDialog, QHBoxLayout, QVBoxLayout, QDialogButtonBox
 
 from mi18n import i18n, i18nc
@@ -33,18 +33,19 @@ class DifferModel(QAbstractTableModel):
         self.diffs = diffs
         self.view = view
 
-    def columnCount(self, unusedIndex:QModelIndex=QModelIndex()) ->int:
+    def columnCount(self, unusedIndex:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->int:
         """how many columns does this node have?"""
         return 3  # rule name, left values, right values
 
-    def rowCount(self, parent:QModelIndex=QModelIndex()) ->int:
+    def rowCount(self, parent:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->int:
         """how many items?"""
         if parent.isValid():
             # we have only top level items
             return 0
         return len(self.diffs)
 
-    def data(self, index:QModelIndex, role:int=Qt.ItemDataRole.DisplayRole) ->Union[int,str,None]:
+    def data(self, index:Union[QModelIndex,QPersistentModelIndex],
+        role:int=Qt.ItemDataRole.DisplayRole) ->Union[int,str,None]:
         """get from model"""
         if not index.isValid():
             return None

src/games.py
@@ -9,9 +9,9 @@ SPDX-License-Identifier: GPL-2.0
 
 import datetime
 
-from typing import Optional, List, Any, TYPE_CHECKING
+from typing import Optional, List, Any, TYPE_CHECKING, Union
 
-from qt import Qt, QModelIndex
+from qt import Qt, QModelIndex, QPersistentModelIndex
 from qt import QAbstractTableModel, QDialogButtonBox, QDialog
 from qt import QHBoxLayout, QVBoxLayout, QCheckBox
 from qt import QItemSelectionModel, QAbstractItemView
@@ -37,11 +37,11 @@ class GamesModel(QAbstractTableModel):
         QAbstractTableModel.__init__(self)
         self._resultRows:List[List[Any]] = []
 
-    def columnCount(self, unusedParent:QModelIndex=QModelIndex()) ->int:
+    def columnCount(self, unusedParent:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->int:
         """including the hidden col 0"""
         return 3
 
-    def rowCount(self, parent:QModelIndex=QModelIndex()) ->int:
+    def rowCount(self, parent:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->int:
         """how many games"""
         if parent.isValid():
             # we have only top level items
@@ -56,7 +56,7 @@ class GamesModel(QAbstractTableModel):
         finally:
             self.endResetModel()
 
-    def index(self, row:int, column:int, parent:QModelIndex=QModelIndex()) ->QModelIndex:
+    def index(self, row:int, column:int, parent:Union[QModelIndex,QPersistentModelIndex]=QModelIndex()) ->QModelIndex:
         """helper"""
         if (row < 0
                 or column < 0
@@ -66,8 +66,9 @@ class GamesModel(QAbstractTableModel):
             return QModelIndex()
         return self.createIndex(row, column, 0)
 
-    def data(self, index:QModelIndex, role:int=Qt.ItemDataRole.DisplayRole) ->Any:
+    def data(self, index:Union[QModelIndex,QPersistentModelIndex], role:int=Qt.ItemDataRole.DisplayRole) ->Any:
         """get score table from view"""
+        assert isinstance(index, QModelIndex)
         if role is None:
             role = Qt.ItemDataRole.DisplayRole
         if not (index.isValid() and role == Qt.ItemDataRole.DisplayRole):

src/genericdelegates.py
@@ -6,23 +6,27 @@ SPDX-License-Identifier: GPL-2.0
 
 """
 
-from typing import TYPE_CHECKING, Optional, Callable, Any, cast
+# mypy: disable_error_code="attr-defined,call-overload"
+# Pyside6 does not seem to have complete/correct annotations for QStyleOptionViewItem
 
-from qt import Qt, QSize, QRect, QEvent
+from typing import TYPE_CHECKING, Optional, Callable, Any, cast, Union
+
+from qt import Qt, QSize, QRect, QEvent, QModelIndex
 from qt import QStyledItemDelegate, QLabel, QTextDocument, QStyle, QPalette, \
     QStyleOptionViewItem, QApplication
 
 from guiutil import Painter
 
 if TYPE_CHECKING:
-    from qt import QLocale, QObject, QPainter, QModelIndex, QAbstractItemModel
+    from qt import QLocale, QObject, QPainter, QPersistentModelIndex, QAbstractItemModel
     from qt import QKeyEvent
+
 
 class ZeroEmptyColumnDelegate(QStyledItemDelegate):
 
     """Display 0 or 0.00 as empty"""
 
-    def displayText(self, value:Any, locale:'QLocale') ->str:
+    def displayText(self, value:Any, locale:'QLocale') ->str:  # type:ignore[override]
         """Display 0 or 0.00 as empty"""
         if isinstance(value, int) and value == 0:
             return ''
@@ -44,8 +48,10 @@ class RichTextColumnDelegate(QStyledItemDelegate):
             self.label.setTextFormat(Qt.TextFormat.RichText)
             self.document = QTextDocument()
 
-    def paint(self, painter:'QPainter', option:QStyleOptionViewItem, index:'QModelIndex') ->None:
+    def paint(self, painter:'QPainter', option:QStyleOptionViewItem,
+        index:Union[QModelIndex,'QPersistentModelIndex']) ->None:
         """paint richtext"""
+        assert isinstance(index, QModelIndex)
         if option.state & QStyle.StateFlag.State_Selected:
             role = QPalette.ColorRole.Highlight
         else:
@@ -59,8 +65,9 @@ class RichTextColumnDelegate(QStyledItemDelegate):
             painter.translate(option.rect.topLeft())
             self.label.render(painter)
 
-    def sizeHint(self, option:QStyleOptionViewItem, index:'QModelIndex') ->QSize:
+    def sizeHint(self, option:QStyleOptionViewItem, index:Union[QModelIndex,'QPersistentModelIndex']) ->QSize:
         """compute size for the final formatted richtext"""
+        assert isinstance(index, QModelIndex)
         text = index.model().data(index)
         self.document.setDefaultFont(option.font)
         self.document.setHtml(text)
@@ -83,8 +90,10 @@ https://wiki.qt.io/Technical_FAQ#How_can_I_align_the_checkboxes_in_a_view.3F"""
         return QApplication.style().pixelMetric(
             QStyle.PixelMetric.PM_FocusFrameHMargin) + 1
 
-    def paint(self, painter:'QPainter', option:QStyleOptionViewItem, index:'QModelIndex') ->None:
+    def paint(self, painter:'QPainter', option:QStyleOptionViewItem,
+        index:Union[QModelIndex,'QPersistentModelIndex']) ->None:
         """paint right aligned checkbox"""
+        assert isinstance(index, QModelIndex)
         viewItemOption = QStyleOptionViewItem(option)
         if self.cellFilter(index):
             textMargin = self.__textMargin()
@@ -101,8 +110,9 @@ https://wiki.qt.io/Technical_FAQ#How_can_I_align_the_checkboxes_in_a_view.3F"""
         QStyledItemDelegate.paint(self, painter, viewItemOption, index)
 
     def editorEvent(self, event:QEvent, model:'QAbstractItemModel',
-        option:QStyleOptionViewItem, index:'QModelIndex') ->bool:
+        option:QStyleOptionViewItem, index:Union[QModelIndex,'QPersistentModelIndex']) ->bool:
         """edit right aligned checkbox"""
+        assert isinstance(index, QModelIndex)
         flags = model.flags(index)
         # make sure that the item is checkable
         if not flags & Qt.ItemFlag.ItemIsUserCheckable or not flags & Qt.ItemFlag.ItemIsEnabled:

commit 5ad57a302ddc340dd68b07c34c7a6f0f241e1889

annotations: Scoring game: fix showing score table
regression from 1e15493604637fe1d98f82477af4ec4910df2aeb

src/scoringdialog.py
@@ -20,6 +20,7 @@ from qt import QAbstractItemView, QHeaderView
 from qt import QTreeView, QFont, QFrame
 from qt import QStyledItemDelegate
 from qt import QBrush, QPalette
+from qt import PYQT6
 from kde import KDialogButtonBox, KApplication
 
 from modeltest import ModelTest
@@ -152,12 +153,12 @@ class ScoreItemDelegate(QStyledItemDelegate):
         index:Union[QModelIndex,'QPersistentModelIndex']) ->None:
         """where the real work is done..."""
         assert painter
-        assert isinstance(index, ScorePlayerItem), \
-            f'ScoreItemDelegate.paint(index={index}): index should be ScorePlayeritem'
+        assert isinstance(index, QModelIndex), index
         item = index.internalPointer()
         if isinstance(item, ScorePlayerItem) and item.parent and item.parent.row() == 3 and index.column() != 0:
             parent_item = cast(TreeItem, index.parent().internalPointer())
             for idx, playerItem in enumerate(parent_item.children):
+                assert isinstance(playerItem, ScorePlayerItem), playerItem
                 rect = option.rect  # type:ignore[attr-defined]
                 chart = cast('ScoreModel', index.model()).chart(rect, index, playerItem)
                 if chart:
@@ -169,7 +170,11 @@ class ScoreItemDelegate(QStyledItemDelegate):
                         # separately per cell beause the lines spread vertically over two rows: We would
                         # have to draw the lines into one big pixmap and copy
                         # from the into the cells
-                        painter.drawPolyline(chart)
+                        if PYQT6:
+                            # seems the annotations are wrong: list(QPointF) does work
+                            painter.drawPolyline(chart)  # type:ignore[call-overload]
+                        else:
+                            painter.drawPolyline(chart)
             return
         QStyledItemDelegate.paint(self, painter, option, index)
 